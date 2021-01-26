35d18bd2b93c071ef5cbbd107bbd8cf5ce27188c
Hiromi Akita <hiromiakitao@gmail.com>
2021-01-25 21:36:57 -0600

    intro js y ejercicios

---
+++ 2_Kata_Js/0_Introduccion/ejercicios.js
@@ -0,0 +1,28 @@
+// Variables
+
+// Guarda en variables el nombre de las siguientes mascotas:
+
+// 1. Pedrito
+// 2. Rufo
+// 3. Chancla
+
+// Muestra en consola, accediendo a las variables, un mensaje como el siguiente:
+// “Mis mascotas son Pedrito, Rufo y Chancla. Les quiero mucho”
+
+// Input y Output
+
+// Pedir mediante prompts el nombre de dos mascotas y mostrar en consola un mensaje como el siguiente:
+
+// “Quiero mucho a mis mascotas mascota1 y mascota2”
+
+// NOTA: mascota1 y mascota2 corresponden a los nombres introducidos mediante prompts
+
+// Conversión de Tipos de Datos
+
+// Guarda la cadena de texto “100” en una variable
+// Muestra el contenido de esta variable en consola
+// Muestra el TIPO DE DATO de esta variable en consola
+// Transforma la variable de string a number
+// Muestra nuevamente el contenido de esta variable en consola
+
+// ¿Qué diferencias notas al mostrar en consola la variable antes y después de transformarla? ¿Qué es entonces un tipo de dato?
