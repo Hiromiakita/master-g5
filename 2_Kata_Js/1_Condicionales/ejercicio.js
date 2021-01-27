1d5757bc3ff846927c5ce98c7a024670f0caa5c3
Hiromi Akita <hiromiakitao@gmail.com>
2021-01-26 20:14:08 -0600

    condicionales

---
+++ 2_Kata_Js/1_Condicionales/ejercicio.js
@@ -0,0 +1,11 @@
+// Ejercicio 1
+
+// Crea una aplicación web empleando JavaScript que permita a dos usuarios jugar una partida de piedra, papel o tijeras.
+
+// Pide al jugador1 y al jugador2 que escriban su elección mediante prompts. Compara los valores ingresados y muestra en pantalla, mediante un alert o console.log, quién fue el jugador ganador.
+
+// Ejercicio 2
+// Crea una aplicación web con JavaScript que reciba tres números digitados por el usuario, mediante prompts, y muestre en pantalla, mediante un alert, cuál es el mayor de los tres números.
+
+// Ejercicio 3
+// Crea una aplicación web con JavaScript que reciba tres números digitados por el usuario, mediante prompts, y muestre en pantalla, mediante un alert, la suma de los tres números.
