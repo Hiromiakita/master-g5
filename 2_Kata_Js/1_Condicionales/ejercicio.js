4144622c60fb5781cd8dbdb42cfef7e0d3603438
Hiromi Akita <hiromiakitao@gmail.com>
2021-01-26 20:34:28 -0600

    ejercicios

--- 2_Kata_Js/1_Condicionales/ejercicio.js
+++ 2_Kata_Js/1_Condicionales/ejercicio.js
@@ -9,3 +9,21 @@
 
 // Ejercicio 3
 // Crea una aplicación web con JavaScript que reciba tres números digitados por el usuario, mediante prompts, y muestre en pantalla, mediante un alert, la suma de los tres números.
+
+
+// Arnoldo le gustan mucho los números y quiere saber más de ellos, él denomina
+// números cool a los que son divisibles entre 3 y 5, el quiere crear un programa en el
+// cual el ingrese un número y le diga si es cool o no, ¿puedes ayudarlo?
+
+// Arnoldo sigue en su búsqueda de números, ahora busca números raros. Los
+// números raros son aquellos que son divisibles entre 5 y entre 3, pero no entre 2. El
+// quiere crear un programa en el cual ingrese un número y le diga si es raro o no.
+
+// Humberto está repartiendo códigos por toda la ciudad. El quiere crear un programa
+// que cuando un usuario ingrese un código este despliegue un mensaje. Para el
+// código happy el quiere desplegar el mensaje “cool bro, the life is short” y para el
+// código sad él quiere, desplegar el mensaje “go for tacos bro, tacos = smile”
+
+// Paco está haciendo un videojuego y necesita hacer un programa que determine la
+// jerarquía de usuario de acuerdo a su nivel, las jerarquías son paladin : nivel 0 -10,
+// caballero dorado : nivel 11 - 30, dios destructor: 31 - 50.
