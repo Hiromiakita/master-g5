e3d864e31491e5824970372b450b4f5f51d5e85d
Hiromi Akita <hiromiakitao@gmail.com>
2021-01-27 20:50:18 -0600

    ejercicios

---
+++ 2_Kata_Js/4_Funciones/ejercicios.js
@@ -0,0 +1,31 @@
+// Hacer una función que convierta pesos a dólares
+
+// Hacer una función que cuente el número de caracteres de una palabra
+
+// Hacer una función que halle el máximo entre dos números 
+
+// Hacer una función que halle el máximo entre tres números
+
+// Hacer una función que calcule el IVA de una cantidad (16%)
+
+// Hacer una función para ingresar cualquier letra del alfabeto 
+// y verifique si es vocal o consonante
+
+// Hacer una función que verifique si un número es positivo o negativo
+
+// Hacer una función que imprima todos los números naturales 
+//en reversa, desde n hasta 1.
+
+// Hacer una función que imprima todos los números pares entre 1 y 100
+
+// Hacer una función que imprima todos los números nones entre 1 y 100
+
+// Hacer una función que halle la suma de todos los números naturales entre 1 y n
+
+// Hacer una función que imprima una pirámide de asteriscos como la siguiente:
+       
+//                      *
+//                     * *
+//                    * * *
+//                   * * * *
+//                  * * * * *
