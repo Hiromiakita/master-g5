742cda3a3f0bec710072441ba6b9e3676f7869f3
Hiromi Akita <hiromiakitao@gmail.com>
2021-02-03 19:45:44 -0600

    prototype

---
+++ 2_Kata_Js/6_Prototype/app.js
@@ -0,0 +1,49 @@
+console.log('hola');
+
+let persona = {
+    edad: 20,
+    pais: 'Mexico',
+    telefono: 3333,
+    saludar: function () {
+        console.log('HOLAA');
+    },
+    correr: function () {
+        console.log('estoy corriendo');
+    }
+};
+
+console.log(persona.edad);
+
+let nombre = 'Luis';
+
+nombre.length;
+
+console.log(nombre);
+
+let numero = 12;
+
+numero.toString();
+
+console.log(numero);
+
+let objeto = {};
+
+console.log(objeto);
+
+let calificaciones = [100, 60, 80, 90];
+
+calificaciones.push(10);
+
+console.log(calificaciones);
+
+
+
+
+
+
+
+
+
+
+
+
