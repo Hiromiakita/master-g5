27388522d744ecb1ecd923ced612a6e812f950bf
Manuel Tejada <manutejada247@gmail.com>
2021-03-02 21:19:12 -0600

    Intro a la logica y conjuntos

--- 2_Kata_Js/8_DOM/main.js
+++ 2_Kata_Js/8_DOM/main.js
@@ -76,114 +76,136 @@
 //     }
 // };
 
-const data = [
-    {
-        id: 1,
-        pic:
-            "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
-        name: "Flat-Twist With Twist Out",
-        difficulty: "Medium",
-        info:
-            "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/",
-    },
-    {
-        id: 2,
-        pic:
-            "https://i1.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/4-natural-short-ponytail-with-twists.jpg?w=476&ssl=1",
-        name: "Updo With Bangs for Short Hair",
-        difficulty: "Easy",
-        info:
-            "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/7/",
-    },
-    {
-        id: 3,
-        pic:
-            "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/2-updo-with-didi-braids.jpg?w=486&ssl=1",
-        name: "Inverted Cornrows",
-        difficulty: "Hard",
-        info:
-            "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/12/",
-    },
-    {
-        id: 4,
-        pic:
-            "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/10-short-tight-natural-curls.jpg?w=451&ssl=1",
-        name: "Straw Set",
-        difficulty: "Medium",
-        info:
-            "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/54/",
-    },
-    {
-        id: 5,
-        pic:
-            "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/7-twisted-roll-and-side-bang.jpg?w=458&ssl=1",
-        name: "Twisted Roll and Side Bang",
-        difficulty: "Medium",
-        info:
-            "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/60/",
-    },
-    {
-        id: 6,
-        pic:
-            "https://i1.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/8-braids-and-double-bun.jpg?w=455&ssl=1",
-        name: "Braids With Double Bun",
-        difficulty: "Easy",
-        info:
-            "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/61/",
-    },
-    {
-        id: 7,
-        pic:
-            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/issa-rae-attends-heavenly-bodies-fashion-the-catholic-news-photo-955825762-1541543191.jpg?crop=0.83341xw:1xh;center,top&resize=980:*",
-        name: "Fulani Braids",
-        difficulty: "Medium",
-        info:
-            "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=3",
-    },
-    {
-        id: 8,
-        pic:
-            "https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/18/1280x1668/wash-and-go.jpg?resize=980:*",
-        name: "Wash and Go",
-        difficulty: "Easy",
-        info:
-            "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=10",
-    },
-    {
-        id: 9,
-        pic:
-            "https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/18/1600x2349/bantu-knots-2.jpg?resize=980:*",
-        name: "Bantu Knots",
-        difficulty: "Easy",
-        info:
-            "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=12",
-    },
-    {
-        id: 10,
-        pic:
-            "https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/18/1280x1759/senegalese-twists-marley-twists.jpg?resize=980:*",
-        name: "Sengalese Twist",
-        difficulty: "Hard",
-        info:
-            "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=24",
-    },
+// const data = [
+//     {
+//         id: 1,
+//         pic:
+//             "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
+//         name: "Flat-Twist With Twist Out",
+//         difficulty: "Medium",
+//         info:
+//             "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/",
+//     },
+//     {
+//         id: 2,
+//         pic:
+//             "https://i1.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/4-natural-short-ponytail-with-twists.jpg?w=476&ssl=1",
+//         name: "Updo With Bangs for Short Hair",
+//         difficulty: "Easy",
+//         info:
+//             "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/7/",
+//     },
+//     {
+//         id: 3,
+//         pic:
+//             "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/2-updo-with-didi-braids.jpg?w=486&ssl=1",
+//         name: "Inverted Cornrows",
+//         difficulty: "Hard",
+//         info:
+//             "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/12/",
+//     },
+//     {
+//         id: 4,
+//         pic:
+//             "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/10-short-tight-natural-curls.jpg?w=451&ssl=1",
+//         name: "Straw Set",
+//         difficulty: "Medium",
+//         info:
+//             "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/54/",
+//     },
+//     {
+//         id: 5,
+//         pic:
+//             "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/7-twisted-roll-and-side-bang.jpg?w=458&ssl=1",
+//         name: "Twisted Roll and Side Bang",
+//         difficulty: "Medium",
+//         info:
+//             "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/60/",
+//     },
+//     {
+//         id: 6,
+//         pic:
+//             "https://i1.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/8-braids-and-double-bun.jpg?w=455&ssl=1",
+//         name: "Braids With Double Bun",
+//         difficulty: "Easy",
+//         info:
+//             "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/61/",
+//     },
+//     {
+//         id: 7,
+//         pic:
+//             "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/issa-rae-attends-heavenly-bodies-fashion-the-catholic-news-photo-955825762-1541543191.jpg?crop=0.83341xw:1xh;center,top&resize=980:*",
+//         name: "Fulani Braids",
+//         difficulty: "Medium",
+//         info:
+//             "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=3",
+//     },
+//     {
+//         id: 8,
+//         pic:
+//             "https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/18/1280x1668/wash-and-go.jpg?resize=980:*",
+//         name: "Wash and Go",
+//         difficulty: "Easy",
+//         info:
+//             "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=10",
+//     },
+//     {
+//         id: 9,
+//         pic:
+//             "https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/18/1600x2349/bantu-knots-2.jpg?resize=980:*",
+//         name: "Bantu Knots",
+//         difficulty: "Easy",
+//         info:
+//             "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=12",
+//     },
+//     {
+//         id: 10,
+//         pic:
+//             "https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/18/1280x1759/senegalese-twists-marley-twists.jpg?resize=980:*",
+//         name: "Sengalese Twist",
+//         difficulty: "Hard",
+//         info:
+//             "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=24",
+//     },
+// ];
+
+// let container = document.getElementById("main");
+
+// for (let i = 0; i < data.length; i++) {
+//     let element = data[i];
+//     let div = document.createElement("div");
+//     let h4 = document.createElement("h4");
+//     let image = document.createElement("img");
+
+//     container.append(div);
+//     div.append(h4);
+//     div.append(image);
+
+//     h4.innerHTML = element.name;
+//     image.src = element.pic;
+//     div.setAttribute("class", "elemento");
+// }
+
+// console.log(document.body);
+
+const cuentas = [
+    { nombre: "Hiromi", saldo: 200, password: "helloworld" },
+    { nombre: "Manuel", saldo: 290, password: "l33t" },
+    { nombre: "Luis", saldo: 67, password: "123" },
 ];
 
-let container = document.getElementById("main");
+const validacion = (user, password) => {
+    cuentas.map((c, i) => {
+        if (c.nombre[i] == user) {
+            if (c.password[i] == password) {
+                console.log("Bienvenido " + c.nombre);
+            } else {
+                console.log("password incorrecto");
+            }
+        } else {
+            console.log("usuario incorrecto");
+        }
+    });
+};
 
-for (let i = 0; i < data.length; i++) {
-    let element = data[i];
-    let div = document.createElement("div");
-    let h4 = document.createElement("h4");
-    let image = document.createElement("img");
-
-    container.append(div);
-    div.append(h4);
-    div.append(image);
-
-    h4.innerHTML = element.name;
-    image.src = element.pic;
-    div.setAttribute("class", "elemento");
-}
-
-console.log(document.body);
+validacion("Luis", "13");
