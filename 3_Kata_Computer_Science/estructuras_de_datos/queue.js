715667acbb3412a9006d88b65b5d72122afc0cc0
hiromiakita <hiromiakitao@gmail.com>
2021-03-17 21:24:04 -0600

    queue class

---
+++ 3_Kata_Computer_Science/estructuras_de_datos/queue.js
@@ -0,0 +1,27 @@
+class Queue {
+    constructor() {
+        this.collection = [];
+    }
+    enqueue() { //recibe nuevo elemento y lo alado a la cola (al final)
+
+    }
+
+    dequeue() { //eliminar el primer elemento de la cola
+
+    }
+
+    length() { //nos regresa el tamaño de la cola
+
+    }
+
+    isEmpty() { //nos regresa una variable booleana de acuerdo a si está o no vacía nuestra cola
+
+    }
+
+    front() { //nos regresa el primer elemento que está en la cola
+
+    }
+}
+
+let filaDeTortillas = new Queue();
+console.log(filaDeTortillas);
