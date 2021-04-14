b888f1bd2af37699a1a7891449a7d394066befe1
hiromiakita <hiromiakitao@gmail.com>
2021-04-13 21:37:00 -0500

    callbacks ejemplos

---
+++ 4_Kata_JS_Advanced/1_Callbacks/ejercicios.js
@@ -0,0 +1,18 @@
+// 1.- Muestra un mensaje en consola mediante un callback. La función que escribas debe poder mostrar el mensaje como console.warn, console.log, console.info, o cualquier método para pintar en consola del objeto console.
+// 2.- Crear una función que reciba como
+//     argumento una variable de cualquier tipo y un callback. La función debe retornar como resultado, mediante el callback, cual es el tipo de dato de la variable ingresada e imprimir su contenido.
+// 3.- Crear una función que reciba como 
+//     argumentos dos números y un callback. Según el callback
+//     que se pase a la función, se devuelve la suma de los
+//     dos números, la resta de los dos números o la
+//     multiplicación de los dos números.
+// 4.- Escribe una función que reciba una cadena de
+//     caracteres y debe devolver, mediante un callback, la
+//     cadena de caracteres en mayúsculas o en minúsculas.
+//             ordenSuperior("PejeLagarto", minus);
+//             -> pejelagarto
+//             ordenSuperior("PejeLagarto", mayus);
+//             -> PEJELARTO
+// 5.- Hacer un arreglo de 4 cantidades de tiempo (en minutos)
+//     EJEMPLO [120, 80, 200, 100] y tomar solo las cantidades
+//     mayores a dos horas (hacer la comparación en horas) regresar el nuevo array mediante un callback.
