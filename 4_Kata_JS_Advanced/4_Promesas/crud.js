e53c3f5bf83e127009ddce6157e9ad01e43443b4
hiromiakita <hiromiakitao@gmail.com>
2021-04-29 16:01:49 -0500

    peticiones

--- 4_Kata_JS_Advanced/4_Promesas/crud.js
+++ 4_Kata_JS_Advanced/4_Promesas/crud.js
@@ -31,12 +31,12 @@ let agregarUsuario = (name, lastName, biography, nacionalidad, gender, age, isAl
     });
 }
 
-agregarUsuario('H', 'A', 'H', 'MX', 'M', 1, false)
-    .then( (body) => {
-        console.log('Usuario añadido exitosamente!!');
-        console.log(body)
-    })
-    .catch( (statusCodeError) => console.log(`ERROR al añadir el usuario. StatusCode: ${statusCodeError}`));
+// agregarUsuario('H', 'A', 'H', 'MX', 'M', 1, false)
+//     .then( (body) => {
+//         console.log('Usuario añadido exitosamente!!');
+//         console.log(body)
+//     })
+//     .catch( (statusCodeError) => console.log(`ERROR al añadir el usuario. StatusCode: ${statusCodeError}`));
 
 //READ -> GET
 
@@ -65,7 +65,7 @@ let leerUsuarios = () => {
     
 // }).catch( error => console.log(error));
 
-let formatearUsuario = (usuario) => {
+let formatearUsuario = usuario => {
     let user =  {
         nombre: usuario.name,
         apellido: usuario.last_name
@@ -76,3 +76,77 @@ let formatearUsuario = (usuario) => {
 //UPDATE
 
 //DELETE
+
+
+//PETICIONES ENCADENADAS
+
+
+//EJEMPLO 1
+let obtenerUsuario = (id) => {
+    const URL = `https://swapi.dev/api/people/${id}`;
+    return new Promise( (resolve, reject) => {
+        request.get(URL, (error, resp, body) => {
+            if(resp.statusCode === 200) {
+                resolve(body);
+            } else {
+                reject(resp.body);
+            }
+        })
+    });
+}
+
+
+let obtenerPelicula = url => {
+    return new Promise ( (res, rej) => {
+        request.get(url, (err, response, body) => {
+            if(response.statusCode === 200) {
+                res(body);
+            } else {
+                rej(response.body);
+            }
+        })
+    })
+}
+
+// obtenerUsuario(1)
+//     .then(body => {
+//         let personajeInfo = JSON.parse(body);
+//         obtenerPelicula(personajeInfo.films[0])
+//             .then(bodyFilm => {
+//                 let infoPeli = JSON.parse(bodyFilm);
+//                 console.log(infoPeli);
+//             })
+//             .catch(msjError => console.log(msjError))
+
+//     })
+//     .catch(msj => console.log(msj));
+
+
+
+// obtenerUsuario(000000)
+//     .then(body => {
+//         let infoUser = JSON.parse(body);
+//         console.log(infoUser)
+//     })
+//     .catch(msjError => console.log(msjError))
+
+// obtenerUsuario(10)
+//     .then( () => console.log('usuario encontrado'))
+//     .catch( () => console.log('usuario no encontrado'));
+
+obtenerUsuario(1)
+    .then(body => {
+        let infoPersonaje = JSON.parse(body);
+        let urlsPeliculas = infoPersonaje.films;
+        urlsPeliculas.forEach(urlPeli => {
+            obtenerPelicula(urlPeli)
+                .then( (bodyPeli) => {
+                    let infoPeli = JSON.parse(bodyPeli);
+                    console.log(infoPeli.title)
+                })
+                .catch( (err) => {
+                    console.log(err);
+                });
+        })
+    })
+    .catch(err => console.log(err));
