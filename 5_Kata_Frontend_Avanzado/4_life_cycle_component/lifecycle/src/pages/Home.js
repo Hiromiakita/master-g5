055b7fd8ac32a6bd6b58c8a8b5081f57293d9143
Manuel Tejada <manuel.tejada95@outlook.com>
2021-06-07 20:17:45 -0600

    List & Keys

--- 5_Kata_Frontend_Avanzado/4_life_cycle_component/lifecycle/src/pages/Home.js
+++ 5_Kata_Frontend_Avanzado/4_life_cycle_component/lifecycle/src/pages/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import List from "../pages/List";
 
 const Home = () => {
     return (
         <div>
-           <h1>Homes</h1> 
+            <List />
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
