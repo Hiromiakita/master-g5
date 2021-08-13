b42283533ae678cc9fff05da380918bbd1aa4711
hiromiakita@gmail.com <hiromiakita@gmail.com>
2021-08-12 20:11:13 -0500

    ejercicios2 express

---
+++ 7_Backend/1_Intro_Express/ejercicios2.js
@@ -0,0 +1,16 @@
+// Crear la base de una API que nos permita lo siguiente:
+
+// Mostrar los productos de una base de datos
+// Mostrar la info de un producto
+// Mostrar las sucursales de una cadena de tiendas
+// Mostrar la info de una sucursal
+// Mostrar los productos existentes por cada sucursal
+// Eliminar una sucursal
+// Eliminar un producto
+// Actualizar info de una sucursal
+// Actualizar precio de un producto 
+// Dar de alta una sucursal
+// Dar de alta un producto
+
+
+// ** Nota: responder con un json que haga sentido con la petición (NO necesariamente data)
