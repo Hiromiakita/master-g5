4e303723c8e6afdcb4eda457e3ddb88f7b8de8b7
Manuel Tejada <manuel.tejada95@outlook.com>
2021-09-29 20:25:09 -0600

    Autenticacion con json web token (JWT)

--- Ejercicios/Kata_Backend/index.js
+++ Ejercicios/Kata_Backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const config = require("./config/config");
 const app = express();
 
 const homeRouter = require("./routes/index");
@@ -7,6 +9,8 @@ const endpointsRouter = require("./routes/endpoints");
 
 require("dotenv").config();
 const PORT = process.env.PORT || "3000";
+
+app.set("key", config.key);
 
 app.use(cors());
 
@@ -21,4 +25,22 @@ app.use("/queries", endpointsRouter);
 
 app.listen(PORT, () => {
     console.log(`Aplicacion corriendo en el puerto ${PORT}`);
+});
+
+app.post("/login", (req, res) => {
+    if (
+        req.body.user === "manutejada@gmail.com" &&
+        req.body.password === "h0l@mund0"
+    ) {
+        const payload = {
+            check: true,
+        };
+        const token = jwt.sign(payload, app.get("key"), {
+            expiresIn: 300,
+        });
+        res.json({
+            response: "Autenticacion exitosa",
+            token: token,
+        });
+    }
 });
--- Ejercicios/Kata_Backend/routes/endpoints.js
+++ Ejercicios/Kata_Backend/routes/endpoints.js
@@ -1,7 +1,30 @@
-var express = require("express");
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+const app = express();
 const endpointsController = require("../controller/endpoints");
+
+app.set("key", config.key);
+
+router.use((req, res, next) => {
+    const token = req.headers["access-token"];
+    if (token) {
+        jwt.verify(token, app.get("key"), (err, decode) => {
+            if (err) {
+                return res.json({
+                    response: "Token no valido",
+                    error: err,
+                });
+            } else {
+                req.decode = decode;
+                next();
+            }
+        });
+    } else {
+        return res.json({ response: "No se ha encontrado un token de acceso" });
+    }
+});
 
 router.get("/", (req, res) => {
     res.send("Ejercicio de Knex");
